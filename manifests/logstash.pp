7084d969238f528e86270e67b0596ed07151cab6
Brandon Rochon <brandon.rochon@gmail.com>
2017-03-15 15:35:10 -0700

    add logstash jar
    Change-Id: If1fc7557b1483379619ce8ebb6f3f53df97787d4

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -7,4 +7,8 @@ class druid::logstash {
     ensure => present,
     source => 'http://central.maven.org/maven2/net/minidev/json-smart/1.1.1/json-smart-1.1.1.jar',
   }
+  archive {"${druid::dist_dir}/lib/jsonevent-layout-2.1.jar":
+    ensure => present,
+    source => 'https://github.com/br0ch0n/log4j-jsonevent-layout/releases/download/jsonevent-layout-2.1/jsonevent-layout-2.1.jar',
+  }
 }
