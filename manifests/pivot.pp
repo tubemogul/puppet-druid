ecfb026d6a3d80f2751b62b842e59ef768fdbc8b
Michael Schiff <michael.schiff@tubemogul.com>
2018-01-09 17:33:29 -0800

    using mysql for state store to enable clustering in imply-ui

--- manifests/pivot.pp
+++ manifests/pivot.pp
@@ -10,7 +10,7 @@
 class druid::pivot (
   $home_dir                     = '/opt/imply/dist/imply-ui',
   $config_dir                   = '/opt/imply/conf/pivot',
-  $state_file                   = '/mnt/pivot/state',
+  $state_connection_string      = '',
   $port                         = 9095,
   $broker_host                  = 'localhost:8082',
   $enable_stdout_log            = true,
