6a259ffeb8da41efdf1b0632014976aeb8944e27
br0ch0n <brandon.rochon@gmail.com>
2020-02-10 14:04:22 -0800

    Update logstash.pp

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -5,7 +5,7 @@
 class druid::logstash {
   archive {"${druid::dist_dir}/lib/json-smart-1.1.1.jar":
     ensure => present,
-    source => 'http://central.maven.org/maven2/net/minidev/json-smart/1.1.1/json-smart-1.1.1.jar',
+    source => 'http://repo1.maven.org/maven2/net/minidev/json-smart/1.1.1/json-smart-1.1.1.jar',
   }
   archive {"${druid::dist_dir}/lib/jsonevent-layout-2.1.jar":
     ensure => present,
